85d01af28fbdcb37d6031dc5f757e86bc2c643ad
zaid.ilyas <zaid.ilyas@ovrlod.com>
2023-05-06 21:27:18 +0500

    Added project files

---
+++ Clean.Site/Controllers/HomeController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.ViewEngines;
+using Microsoft.Extensions.Logging;
+using Umbraco.Cms.Core.Web;
+using Umbraco.Cms.Web.Common.Controllers;
+
+namespace Clean.Site.Controllers
+{
+    public class HomeController : RenderController
+    {
+        public HomeController(ILogger<RenderController> logger, ICompositeViewEngine compositeViewEngine, IUmbracoContextAccessor umbracoContextAccessor)
+            : base(logger, compositeViewEngine, umbracoContextAccessor)
+        {
+        }
+
+        public override IActionResult Index()
+        {
+            ViewBag.title = CurrentPage.GetProperty("title").GetValue();
+            return View("~/Views/Home.cshtml");
+        }
+    }
+}
